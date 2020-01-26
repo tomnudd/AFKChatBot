63807a6be325e68db4eff9cb735d7d141d91e5a2
Tom <thomas.g.nudd@durham.ac.uk>
2020-01-26 01:01:17 +0000

    dialogflow integration

--- coreapi.py
+++ coreapi.py
@@ -10,6 +10,33 @@ from google.auth.transport.requests import Request
 import fbchat
 from fbchat import Client
 
+import json
+
+import dialogflow
+from google.api_core.exceptions import InvalidArgument
+
+from google.oauth2 import service_account
+dialogflow_key = json.load(open('dump-171ae8362e6a.json'))
+credentials = (service_account.Credentials.from_service_account_info(dialogflow_key))
+session_client = dialogflow.SessionsClient(credentials=credentials)
+
+DIALOGFLOW_PROJECT_ID = 'jimmys4life-egdwhn'
+SESSION_ID = '8921672436578789054'
+session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+
+text_to_be_analyzed = "how is you?"
+text_input = dialogflow.types.TextInput(text=text_to_be_analyzed, language_code="en-GB")
+query_input = dialogflow.types.QueryInput(text=text_input)
+
+try:
+    response = session_client.detect_intent(session=session, query_input=query_input)
+except InvalidArgument:
+    print("ok")
+print("Query text:", response.query_result.query_text)
+print("Detected intent:", response.query_result.intent.display_name)
+print("Detected intent confidence:", response.query_result.intent_detection_confidence)
+print("Fulfillment text:", response.query_result.fulfillment_text)
+
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 def calendar():
